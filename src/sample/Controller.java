39f4f29e9a204a9ef767008d77cdc6b682a2a75b
willriggins <williamriggins2013@gmail.com>
2016-05-31 12:00:33 -0400

    initial

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,19 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable, ChangeListener {
 
     @FXML
     TextField addressBar;
@@ -29,4 +35,31 @@ public class Controller {
             onGo();
         }
     }
+
+    public void onBack() {
+        try {
+            view.getEngine().getHistory().go(-1);
+        } catch (Exception e) {}
+        }
+
+
+    public void onForward() {
+        try {
+            view.getEngine().getHistory().go(1);
+        } catch (Exception e) {}
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        view.getEngine().getLoadWorker().stateProperty().addListener(this);
+    }
+
+    @Override
+    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+        String address = view.getEngine().getLocation();
+        addressBar.setText(address);
+    }
+
 }
+
